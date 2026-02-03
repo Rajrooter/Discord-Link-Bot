af259b14dcc6b126a198c02ffae557b8c8d3d4f3
Raj Aryan <rajaryan16610@gmail.com>
2026-02-03 14:05:00 +0530

    Make run function port configurable and daemonize thread

--- main.py
+++ main.py
@@ -2112,10 +2112,11 @@ def home():
     return "Bot is alive!"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.environ.get("PORT", "8080"))
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
-    t = Thread(target=run)
+    t = Thread(target=run, daemon=True)
     t.start()
 keep_alive()
 @bot.event
